8fe710bdf02ac999629224b7b6939cfb12cfdbb7
GillsEffect <mustafag0000@gmail.com>
2019-07-05 15:57:11 +0300

    Initial commit

--- girlsdjango/mysite/urls.py
+++ girlsdjango/mysite/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('blog.urls')),
+    url(r'^', include('blog.urls')),
 ]
